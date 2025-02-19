f2ab61c33903f680a76da63f2f5a7901fa649e8e
Hasinikadali <kadalihasini1930@gmail.com>
2025-02-19 09:24:13 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
 let products = []
+
 
 fetch("products.json")
   .then((response) => response.json()) 
@@ -70,7 +70,7 @@ const showProducts = (data) => {
     <img src='${value.url}'>
     <h3>${value.name}</h3>
     <p>${value.desc}</p>
-    <h4>${value.price}</h4>
+    <h4>$${value.price}</h4>
     <button onclick='addToCart(${value.id})'>Add to Cart</button>
     </div>
     `;
